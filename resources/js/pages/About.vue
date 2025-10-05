ed40ba03763e80d2bc9b476d29265800bb3935d4
oposchat <oposchatsl@gmail.com>
2025-10-05 11:59:00 +0200

    Update About.vue

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -14,13 +14,13 @@ import SiteFooter from '@/components/SiteFooter.vue';
             <p class="text-gray-600 mt-3">
                Oposchat nace de estudiantes por y para estudiantes. Somos un grupo de amigos de carrera y durante los años en los que no paramos de estudiar , nos percatamos de la ineficiencia de nuestros métodos de estudio. Cientos de horas empleadas para finalmente no plasmar todo ese ese esfuerzo en los exámenes finales. 
 
-Hacía final de carrera comenzaron a emerger las primeras inteligencias artificiales y rápidamente las incorporamos a nuestros hábitos de estudio, dándonos cuenta de que reteníamos más en menos tiempo .
+Hacia final de carrera comenzaron a emerger las primeras inteligencias artificiales y rápidamente las incorporamos a nuestros hábitos de estudio, dándonos cuenta de que reteníamos más en menos tiempo.
 
-Así nace oposchat, de la necesidad de 4 amigos de encontrar una forma de estudio rápida , eficiente y entretenida.
+Así nace oposchat, de la necesidad de 4 amigos de encontrar una forma de estudio rápida, eficiente y entretenida.
 
-Desde oposchat , integramos el  mejor temario junto con nuestro chat impulsado por IA, especialmente diseñada para el estudio de oposiciones .
+Desde oposchat, integramos el  mejor temario junto con nuestro chat impulsado por IA, especialmente diseñada para el estudio de oposiciones .
 
-Prueba nuestro servicio y empieza a disfrutar de las ventajas que oposchat tiene para ti .
+Prueba nuestro servicio y empieza a disfrutar de las ventajas que oposchat tiene para ti.
             </p>
 
             <section class="mt-8 grid md:grid-cols-3 gap-6">
